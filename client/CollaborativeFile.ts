9f6e3aa1083382569791463805ad147517796a0a
Troubluson <h.niskanen99@gmail.com>
2024-03-06 18:18:39 +0200

    feat: Create document logic and initial websocket

---
+++ client/CollaborativeFile.ts
@@ -0,0 +1,27 @@
+
+
+
+/*
+type CollaborativeDocument = {
+    creatorId: string,
+    documentId: string,
+    channelId: string,
+    documentName: string,
+    revisionHistory: DocumentRevision[]
+    documentParagraphs: string // the paragraphs of the document
+}
+
+type DocumentRevision = {
+    version: number,
+    timeStamp: number,
+    editor: User
+    documentId: string,
+    operations: Operation[]
+}
+
+type Operation = {
+    type: "ADD"|"Remove",
+    startIndex: number,
+    content: string
+}
+*/
