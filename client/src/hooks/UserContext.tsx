eff220c346f0f6932ea100072e5c66556f58e18e
Johannes Peltola <johannes.peltola@codebite.fi>
2024-03-19 13:22:22 +0200

    Improved latency diagnostics accuracy

--- client/src/hooks/UserContext.tsx
+++ client/src/hooks/UserContext.tsx
@@ -1,7 +1,9 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import { IUser, IUserContext } from '../@types/User';
 import Login from '../components/Login/Login';
-import { pingLatency } from '../components/Diagnostics/Measurement';
+import useWebSocket from 'react-use-websocket';
+import { WS_BASE_URL } from '../config';
+import { reportLatency } from '../components/Diagnostics/Measurement';
 
 export const UserContext = createContext<IUserContext>({
   user: null,
@@ -22,14 +24,30 @@ export const UserProvider = ({ children }: Props) => {
     setUser(null);
   };
 
+  const { sendJsonMessage, readyState } = useWebSocket(`${WS_BASE_URL}/latency`, {
+    onOpen: () => console.log('websocket opened'),
+    shouldReconnect: () => false,
+    onMessage: ({ data }) => {
+      if (!user) return;
+      const end_time = new Date();
+      const start_time = new Date(JSON.parse(data).data.start_time);
+      const latency = Number(end_time) - Number(new Date(start_time));
+      reportLatency(user.id, latency);
+    },
+  });
+
   useEffect(() => {
-    if (!user) {
-      return;
-    }
-    pingLatency(user.id);
-    //Ping every 30s
-    setInterval(() => pingLatency(user.id), 30000);
-  }, [user]);
+    if (!user || !readyState) return;
+    //Ping every 15s
+    setInterval(
+      () =>
+        sendJsonMessage({
+          event: 'ping',
+          data: { start_time: new Date() },
+        }),
+      15000,
+    );
+  }, [user, readyState]);
 
   return (
     <UserContext.Provider
